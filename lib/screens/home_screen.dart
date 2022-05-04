1223c31a909bd6eb5a62b0761cdc67d6d2066aca
Claudio Roldán Ramos <claudio.roldan@atos.net>
2022-05-04 11:22:29 +0100

    feat(ui): widgets + styles 💁‍♂️

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/product_card.dart';
 
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Productos'),
+      ),
+      body: ListView.builder(itemCount: 10, itemBuilder: (BuildContext context, int index) => ProductCard()),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
