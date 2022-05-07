f529386df6ef613fd0516a22ac5761301d153657
Claudio Roldán Ramos <claudio.roldan@atos.net>
2022-05-07 12:30:26 +0100

    feat + fix : wip providers + fix .gif 🛠️

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/screens/screens.dart';
+import 'package:productos_app/services/services.dart';
 import 'package:productos_app/widgets/product_card.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final productsService = Provider.of<ProductsService>(context);
+
+    if (productsService.isLoading) return const LoadingScreen();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Productos'),
       ),
       body: ListView.builder(
-        itemCount: 10, 
-        itemBuilder: (BuildContext context, int index) => GestureDetector(
-          child: const ProductCard(),
-          onTap: () => Navigator.pushNamed(context, 'product'),
-          
-          )),
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) => GestureDetector(
+                child: const ProductCard(),
+                onTap: () => Navigator.pushNamed(context, 'product'),
+              )),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {},
---
+++ lib/services/products_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:productos_app/models/models.dart';
+import 'package:http/http.dart' as http;
+
+class ProductsService extends ChangeNotifier {
+  final String _baseUrl = 'flutter-varios-4a4a1-default-rtdb.europe-west1.firebasedatabase.app';
+
+  final List<Product> products = [];
+
+  bool isLoading = true;
+
+  ProductsService() {
+    loadProducts();
+  }
+
+  Future<List<Product>> loadProducts() async {
+    isLoading = true;
+    notifyListeners();
+
+    final url = Uri.https(_baseUrl, 'products.json');
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> productsMap = json.decode(resp.body);
+
+    productsMap.forEach((key, value) {
+      final tempProduct = Product.fromMap(value);
+      tempProduct.id = key;
+      products.add(tempProduct);
+    });
+
+    isLoading = false;
+    notifyListeners();
+
+    return products;
+  }
+}
